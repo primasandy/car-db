ec3a0b859330cc30ecc0873a736d4048b017d0f7
primasandy <40583559+primasandy@users.noreply.github.com>
2025-01-19 03:26:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Pastikan base URL sesuai
+  base: './', // Gunakan './' agar path relatif
   build: {
     outDir: "dist", // Direktori hasil build
   },
